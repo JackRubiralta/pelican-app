3461fa7834421de62da35cd6222e961d41f8cb7f
Jack Rubiralta <jackrubiralta@outlook.com>
2024-04-09 15:04:22 -0400

    Again fixed icons

--- src/pages/Games.js
+++ src/pages/Games.js
@@ -1,3 +1,31 @@
+
+
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const GamesPage = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.messageText}>Not implemented yet. App by Jack Rubiralta</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  messageText: {
+    color: 'black',
+    textAlign: 'center',
+  },
+});
+
+export default GamesPage;
+/*
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { useNavigation } from "@react-navigation/native";
@@ -60,3 +88,4 @@ const styles = StyleSheet.create({
 });
 
 export default GamesPage;
+*/
